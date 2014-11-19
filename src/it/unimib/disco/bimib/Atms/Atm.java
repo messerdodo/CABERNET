cc7576b4f479c2a9edf189e0aab6b1f8a5cca416
Andrea Paroni <a.paroni@campus.unimib.it>
2014-11-19 12:07:28 +0100

    Feature added
    Feature added for the stats calculation

---
+++ src/it/unimib/disco/bimib/Atms/Atm.java
@@ -0,0 +1,420 @@
+/**
+ * This class creates the ATM matrix
+ * 
+ * @author Andrea Paroni (a.paroni@campus.unimib.it)
+ * @author Giorgia Previtali (g.previtali6@campus.unimib.it)
+ * @group BIMIB @ DISCo (Department of Information Technology, Systems and Communication) of Milan University - Bicocca 
+ * @year 2014
+ * 
+ */
+
+package it.unimib.disco.bimib.Atms;
+
+//System imports
+import java.util.ArrayList;
+
+
+//GRNSim imports
+import it.unimib.disco.bimib.Exceptions.*;
+import it.unimib.disco.bimib.Mutations.Mutation;
+import it.unimib.disco.bimib.Sampling.AttractorsFinder;
+import it.unimib.disco.bimib.Statistics.DynamicPerturbationsStatistics;
+import it.unimib.disco.bimib.Utility.SCCTarjan;
+import it.unimib.disco.bimib.Utility.UtilityRandom;
+
+
+public class Atm {
+
+	//References to attractor's interface and mutation's interface
+	private AttractorsFinder attractorsFinder;
+	private Mutation mutation;
+	private boolean dynamicPerturbsComputation;
+	private DynamicPerturbationsStatistics dynamicPerturbationsStatistics;
+
+	//Initializes the atm
+	private double[][] atm;
+	//Initializes the frequency of the attractors
+	private int[] totalFrequencyRows;
+	private ArrayList<double[][]> storedAtmNotMatching = new ArrayList<double[][]>();
+
+	/**
+	 * This is the constructor where there is initialized the attractor's interface
+	 * and the call for the main method
+	 * @param attractorsFinder: the attractor finder object
+	 * @param mutation: The mutation object as initialized for the task.
+	 * @param dynamicPerturbsComputation: boolean value to specify if compute the avalanches and the sensitivity tasks. 
+	 * @param nodes: number of nodes if the network
+	 * @throws NotExistingAttractorsException 
+	 * @throws ParamDefinitionException 
+	 * @throws Exception 
+	 */	
+	public Atm(AttractorsFinder attractorsFinder, Mutation mutation, boolean dynamicPerturbsComputation, int nodes){
+		//Parameters checking
+		if(attractorsFinder == null)
+			throw new NullPointerException("Attractor finder object must be not null");
+		if(mutation == null)
+			throw new NullPointerException("Mutation object must be not null");
+
+		this.attractorsFinder = attractorsFinder;
+		this.mutation = mutation;
+		this.dynamicPerturbsComputation = dynamicPerturbsComputation;
+		if(this.dynamicPerturbsComputation)
+			this.dynamicPerturbationsStatistics = new DynamicPerturbationsStatistics(nodes);
+		else
+			this.dynamicPerturbationsStatistics = null;
+	}
+
+	/**
+	 * This is the constructor where there is initialized the attractor's interface
+	 * and the call for the main method
+	 * @param attractorsFinder: the attractor finder object
+	 * @param mutation: The mutation object as initialized for the task.
+	 * @throws NotExistingAttractorsException 
+	 * @throws ParamDefinitionException 
+	 * @throws Exception 
+	 */	
+	public Atm(AttractorsFinder attractorsFinder, Mutation mutation){
+		this(attractorsFinder, mutation, false, -1);
+	}
+
+	/**
+	 * Generic constructor
+	 * @param attractorsFinder: An attractor finder object
+	 * @param mutation: A mutation object
+	 * @param atmMatrix: A not normalized atm matrix 
+	 */
+	public Atm(AttractorsFinder attractorsFinder, Mutation mutation, double[][] atmMatrix){
+		if(atmMatrix == null)
+			throw new NullPointerException("Atm matrix must be not null");
+		if(attractorsFinder == null)
+			throw new NullPointerException("Attractor finder must be not null");
+		if(mutation == null)
+			throw new NullPointerException("Mutation object must be not null");
+
+		this.atm = atmMatrix;
+		this.attractorsFinder = attractorsFinder;
+		this.mutation = mutation;
+		//Normalizes the atm matrix
+		this.normalize();
+
+	}
+
+	public void createAtm(Object[] attractors, int perturbExperiments, double perturbStatesRatio) 
+			throws MissingFeaturesException, ParamDefinitionException, NotExistingAttractorsException, 
+			NotExistingNodeException, InputTypeException, AttractorNotFoundException {
+
+		//Param check
+		if(perturbStatesRatio < 0 || perturbStatesRatio > 1)
+			throw new ParamDefinitionException("The ratio of states to perturb value must be in [0, 1] interval");
+
+		int numberOfAttractors = attractors.length;
+		Object newState, attractorNewState;
+		Object[] statesInAttractor;
+
+		//Copies the attractor's array into an ArrayList
+		ArrayList<Object> attractorVet = new ArrayList<Object>();
+		for(int index = 0; index < numberOfAttractors; index++){
+			attractorVet.add(attractors[index]);
+		}
+		//Initializes the atm.
+		this.atm = new double[numberOfAttractors][numberOfAttractors];
+
+		//Initializes the atm at zero
+		for(int line = 0; line < numberOfAttractors; line++){
+			for(int pillar = 0; pillar < numberOfAttractors; pillar++){
+				this.atm[line][pillar] = 0;
+			}
+		}
+
+		//Calculates the ATM entries.
+		int index;
+
+		for(int a = 0; a < numberOfAttractors; a++){
+
+			//Gets a permutation of the states in the selected attractor
+			statesInAttractor = UtilityRandom.randomPermutation(
+					this.attractorsFinder.getStatesInAttractor(attractorVet.get(a)));
+			index = 0;
+			Object state;
+			do{
+				state = statesInAttractor[index];
+				//Perform the perturb experiment
+				for(int exp = 0; exp < perturbExperiments; exp++){
+					//Calls the mutation method 
+					newState = this.mutation.doMutation(state);
+					//Gets the new state's attractor
+					attractorNewState = attractorsFinder.getAttractor(newState);
+
+					//Computes the avalanches and the sensitivity if required.
+					if(this.dynamicPerturbsComputation){
+						dynamicPerturbationsStatistics.avalanchesAndSensitivityComputation(statesInAttractor, 
+								this.attractorsFinder.getStatesInAttractor(attractorNewState));
+					}
+
+					if(attractorNewState != null){
+
+						//Verifies if the attractor has already been discovered.
+						if(attractorVet.contains(attractorNewState) == true){
+
+							//Gets the new state's index from the attractor's ArrayList
+							//and modifies the Atm matrix
+							this.atm[a][attractorVet.indexOf(attractorNewState)] = this.atm[a][attractorVet.indexOf(attractorNewState)] + 1.0;
+						}else{
+							//Adds the new attractors and modifies the Atm matrix
+							reCreateAtm(attractorsFinder.getAttractors());
+							attractorVet.add(attractorNewState);
+							this.atm[a][attractorVet.indexOf(attractorNewState)] = this.atm[a][attractorVet.indexOf(attractorNewState)] + 1.0;
+							numberOfAttractors = numberOfAttractors + 1;
+						}
+					}else{
+						System.out.println("No attractor found");
+					}
+				}
+				index ++;
+
+			}while(index < Math.floor(perturbStatesRatio * statesInAttractor.length));	
+		}
+
+		//Atm matrix normalization
+		this.normalize();
+		storeAtmNotMatching(this.atm);
+	}
+
+	/**
+	 * This method recreates the Atm matrix when for a state there isn't its attractor
+	 * @param newAttractors : an array with all the attractors of the network
+	 * @throws NotExistingAttractorsException
+	 */
+	private void reCreateAtm(Object[] newAttractors) throws NotExistingAttractorsException{
+
+		int numbersOfNewAttractors = newAttractors.length;
+
+		//Creates the new Atm matrix
+		double[][] newAtm = new double[numbersOfNewAttractors][numbersOfNewAttractors];
+
+		//Recopies all the values from the old Atm matrix
+		for(int line = 0; line < numbersOfNewAttractors-1; line++){
+			for(int pillar = 0; pillar < numbersOfNewAttractors-1; pillar++){
+				newAtm[line][pillar] = this.atm[line][pillar];
+			}
+		}
+
+		//Adds the values for the new pillar
+		for(int pillar = 0; pillar < numbersOfNewAttractors; pillar++){
+
+			newAtm[numbersOfNewAttractors -1][pillar] = 0;
+		}
+
+		//Adds the values for the new line
+		for(int line = 0; line < numbersOfNewAttractors; line++){
+			newAtm[line][numbersOfNewAttractors -1] = 0;
+		}
+
+		//Initializes the new Atm
+		this.atm = newAtm;
+
+	}
+
+	/**
+	 * This method return the Atm matrix
+	 * @return Atm
+	 */
+	public double[][] getAtm(){
+		return this.atm;
+	}
+
+	/**
+	 * This method returns a copy of the Atm matrix
+	 * @return
+	 */
+
+	public double[][] copyAtm(){
+		double[][] atmForTes = new double[atm.length][atm.length];
+
+		for(int line = 0; line < atm.length; line++){
+			for(int pillar = 0; pillar < atm.length; pillar++){
+				atmForTes[line][pillar] = this.atm[line][pillar];
+			}
+		}
+		return atmForTes;
+	}
+
+	/**
+	 * This method returns a copy of the atm matrix without the links
+	 * less than delta.
+	 * @param delta
+	 * @return a copy of the atm matrix with some links removed.
+	 */
+	public double[][] copyAtmMatrixWithDeltaRemoval(double delta){
+		double[][] copiedDelta = new double[atm.length][atm.length];
+		//Scans the original atm matrix removing the links with the 
+		// associated value less than delta
+		for(int i = 0; i < atm.length; i++){
+			for(int j = 0; j < atm.length; j++){
+				copiedDelta[i][j] = (atm[i][j] < delta ? 0 : atm[i][j]);
+			}
+		}
+		return copiedDelta;
+	}
+
+	/**
+	 * This method returns the atm matrix not normalized
+	 * @return the atm matrix not normalized
+	 */
+	public double[][] getAtmWithoutNormalization(){
+		double[][] atmNotNormalized = new double[this.atm.length][this.atm.length];
+
+		for(int line = 0; line < this.atm.length; line++){
+			//Checks if the total frequency of the specified line is 0
+			if(this.totalFrequencyRows[line] == 0){
+				for(int pillar = 0; pillar < this.atm.length; pillar ++){
+					//Multiplies the atm value for the number of attractors
+					atmNotNormalized[line][pillar] = this.atm[line][pillar] * this.attractorsFinder.getAttractors().length; 
+				}
+			}else{
+				for(int pillar = 0; pillar < this.atm.length; pillar ++){
+					//Multiplies the atm value for the total frequency of the specified line 
+					atmNotNormalized[line][pillar] = this.atm[line][pillar] * this.totalFrequencyRows[line]; 
+				}
+			}
+		}
+		return atmNotNormalized;
+	}
+
+	/**
+	 * This method stores the not matching atms
+	 * @param atm the atm to store.
+	 */
+	public void storeAtmNotMatching(double[][] atm){
+		storedAtmNotMatching.add(atm);
+	}
+
+	/**
+	 * This method returns the stored not matching atms
+	 * @return Atms
+	 */
+	public ArrayList<double[][]> getStoredAtmNotMaching(){
+		return storedAtmNotMatching;
+	}
+
+	/***
+	 * This method returns the ATM in the tsv format
+	 */
+	public String getCsvAtm(){
+		String csvAtm = "";
+		//Converts the ATM in a tsv format
+		for(int i = 0; i < this.atm.length; i++){
+			//New line
+			if(i != 0)
+				csvAtm += "\n";
+			for(int j = 0; j < this.atm.length - 1; j++){
+				csvAtm += this.atm[i][j] + ",";
+			}
+			csvAtm += this.atm[i][this.atm.length-1];
+		}
+
+		return csvAtm;
+	}
+
+	/**
+	 * This method normalizes the atm matrix.
+	 */
+	private void normalize(){
+		this.totalFrequencyRows = new int[this.atm.length];
+		int numberOfAttractors =  this.atm.length;
+
+		for(int line = 0; line < this.atm.length; line++){
+			this.totalFrequencyRows[line] = 0;
+
+			for(int pillar = 0; pillar < this.atm.length; pillar++){
+				//Calculates the frequency for the specified line
+				this.totalFrequencyRows[line] = this.totalFrequencyRows[line] + (int)this.atm[line][pillar];
+			}
+
+			for(int pillar = 0; pillar < this.atm.length; pillar++){
+				//Normalizes the Atm matrix
+				if(this.totalFrequencyRows[line] == 0)
+					this.atm[line][pillar] = 1.0/numberOfAttractors;
+				else
+					this.atm[line][pillar] = this.atm[line][pillar]/this.totalFrequencyRows[line];
+			}
+		}
+	}
+
+	/**
+	 * This method returns the DynamicPerturbationStatistics object.
+	 * @return the DynamicPerturbationStatistics object.
+	 */
+	public DynamicPerturbationsStatistics getDynamicPerturbationsStatistics(){
+		return this.dynamicPerturbationsStatistics;
+	}
+
+	/**
+	 * This method returns the number of TESes for a given threshold
+	 * @param threshold: The transition probability threshold. It must be between 0 and 1;
+	 * @return The number of TES.
+	 */
+	public int getTesNumber(double threshold){
+		int tes = 0;
+		int[] assignments, temporaryTesSet;
+		ArrayList<ArrayList<Integer>> supportGraph = new ArrayList<ArrayList<Integer>>();
+		//Param checking
+		if(threshold < 0.0){
+			threshold = 0.0;
+		}else if(threshold > 1.0){
+			threshold = 1.0;
+		}
+
+		//Creates the atm graph
+		for(int i = 0; i < this.atm.length; i++){
+			supportGraph.add(i, new ArrayList<Integer>());
+			for(int j = 0; j < this.atm.length; j++){
+				if(this.atm[i][j] >= threshold){
+					supportGraph.get(i).add(j);
+				}
+			}
+		}
+
+		//Computes the SCC
+		SCCTarjan scc = new SCCTarjan();
+		ArrayList<ArrayList<Integer>> aux = scc.scc(supportGraph);
+
+		//Creates the assignments array. 
+		//Each position of the array contains the number of the scc of the element.
+		assignments = new int[this.atm.length];
+		for(int i  = 0; i < aux.size(); i++){
+			for(Integer att : aux.get(i))
+				assignments[att] = i;
+		}
+
+		//All the scc are teses at the beginning.
+		temporaryTesSet = new int[aux.size()];
+		for(int i = 0; i < aux.size(); i++)
+			temporaryTesSet[i] = 1;
+		int j = 0;
+
+		//Removes the scc that are not tes.
+		for(int i = 0; i < this.atm.length; i++){
+			j = 0;
+			while((j < this.atm.length) && (this.atm[i][j] == 0 || (!((this.atm[i][j] >= threshold) && (assignments[i] != assignments[j])))) ){
+				j = j + 1;
+			}
+			if(j < this.atm.length){
+				temporaryTesSet[assignments[i]] = 0;
+			}
+		}
+
+		//Computes the number of teses.
+		for(int i = 0; i < temporaryTesSet.length; i++)
+			tes = tes + temporaryTesSet[i];
+
+		return tes;
+	}
+
+	
+	public Atm(double[][] atm){
+		this.atm = atm;
+	}
+
+
+}
---
+++ src/it/unimib/disco/bimib/Statistics/DynamicalStatistics.java
@@ -0,0 +1,97 @@
+package it.unimib.disco.bimib.Statistics;
+
+//GESTODifferent imports
+import it.unimib.disco.bimib.Exceptions.InputTypeException;
+import it.unimib.disco.bimib.Exceptions.NotExistingNodeException;
+import it.unimib.disco.bimib.Exceptions.ParamDefinitionException;
+import it.unimib.disco.bimib.Sampling.SamplingManager;
+
+//System imports
+import java.util.ArrayList;
+
+public class DynamicalStatistics {
+	
+	private ArrayList<Integer> attractorsLengths;
+	private ArrayList<Integer> basinOfAttraction;
+	
+	private SamplingManager samplingManager;
+	
+	public DynamicalStatistics(SamplingManager samplingManager){
+		this.attractorsLengths = new ArrayList<Integer>();
+		this.basinOfAttraction = new ArrayList<Integer>();
+		this.samplingManager = samplingManager;
+	}
+
+	/**
+	 * This method returns an Array List with all the attractors lengths.
+	 * The array is not computed when the method is invoked, but is returned the stored version.
+	 * @return all the attractors lengths.
+	 * @throws ParamDefinitionException
+	 * @throws NotExistingNodeException
+	 * @throws InputTypeException
+	 */
+	public ArrayList<Integer> getAttractorsLength() throws ParamDefinitionException, NotExistingNodeException, InputTypeException{
+		return this.getAttractorsLength(false);
+	}
+	
+	/**
+	 * This method returns an Array List with all the attractors lengths.
+	 * If the compute param is false, the array is not computed when the method is invoked, but is returned the stored version.
+	 * Otherwise if the param compute is true, the array is computed and it will be updated.
+	 * @param compute: It used to specify if the attractor lengths must be computed.
+	 * @return all the attractors lengths.
+	 * @throws ParamDefinitionException
+	 * @throws NotExistingNodeException
+	 * @throws InputTypeException
+	 */
+	public ArrayList<Integer> getAttractorsLength(boolean compute) throws ParamDefinitionException, NotExistingNodeException, InputTypeException{
+		if(compute){
+			//Computes a new version of the attractors lenght
+			Object[] attractors;
+			this.attractorsLengths = new ArrayList<Integer>();
+			//Gets the array
+			attractors = this.samplingManager.getAttractorFinder().getAttractors();
+			for(Object attractor : attractors){
+				//Gets the attractor length
+				this.attractorsLengths.add(this.samplingManager.getAttractorFinder().getAttractorLength(attractor));
+			}
+		}
+		//Returns the attractors lengths
+		return this.attractorsLengths;
+	}
+	
+	/**
+	 * This method returns the basin of attraction of each attractor of the network.
+	 * The basin of attraction is not computed when the method is invoked.
+	 * @return The basin of attraction of each attractor of the network.
+	 */
+	public ArrayList<Integer> getBasinOfAttraction(){
+		return this.getBasinOfAttraction(false);
+	}
+	
+	/**
+	 * This method returns the basin of attraction of each attractor of the network.
+	 * If the parameter compute is true, the method compute the basin of attraction when it is invoked.
+	 * Otherwise, if compute is false, the method returns the stored array, so it is not update.
+	 * @param compute: It is used in order to specify id the basin of attraction must be compute.
+	 * @return an array with the basin of attraction of each attractor of the network.
+	 */
+	public ArrayList<Integer> getBasinOfAttraction(boolean compute){
+		if(compute){
+			//Gets the updated basin of attraction of each attractor
+			this.basinOfAttraction = this.samplingManager.getAttractorFinder().getBasinOfAttraction();
+		}
+		return this.basinOfAttraction;
+	}
+	
+	/**
+	 * This method returns the ratio of oscillating nodes. 
+	 * An oscillating node of a network is a node that change it's value
+	 * in the states of an attractor.
+	 * @return
+	 */
+	public double getOscillatingNodesRatio(){
+		return this.samplingManager.getAttractorFinder().getOscillatingNodesRatio();
+	}
+	
+}
